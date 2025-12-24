1d30b1fb04b30dc4e98651a04bf0859f96bf92ca
Jerryc6 <jerryclanuevo27@gmail.com>
2025-12-24 00:24:21 -0500

    Commited

--- projFile/com/GUI/WindowFrame.java
+++ projFile/com/GUI/WindowFrame.java
@@ -1,17 +1,48 @@
 package projFile.com.GUI;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class WindowFrame extends JFrame{
-    JTextField user = new JTextField();
 
-    public WindowFrame(){
-        setTitle("Windows");
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
+public class WindowFrame{
+    
+
+    public void Body(){
+    
+	    SwingUtilities.invokeLater(() ->{
+	    
+		    JFrame frame = new JFrame("Inventory");
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.setPreferredSize(new Dimension(1200,700));
+
+		    frame.setLayout(new BorderLayout());
+
+		    JPanel upperPanel. = new JPanel(new FlowLayout());
+		    frame.add();
+
+
+
+	    
+	    
+	    
+
+
+
+
+		    frame.pack();
+		    frame.setLocationRelativeTo(null);
+		    frame.setVisible(true);
+	    });
+    
     }
 
 
